87ec6494ed5113e043f1a6f7cdb933679be3b274
JamesHoffman66 <jhoffm12@student.fitchburgstate.edu>
2025-04-12 13:41:04 -0400

    Added player movement and color to distinguish parts of the scene

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float horizontalInput;
+    public float verticalInput;
+    private float speed = 20f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //player movement 
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+    }
+}
