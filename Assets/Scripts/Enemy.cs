ff0691b702e1b504de59e843d55cfc9be9b6f671
JamesHoffman66 <jhoffm12@student.fitchburgstate.edu>
2025-04-16 20:12:27 -0400

    Successfully implemented bounce back
    Made it so when the enemy hits the player character, the player is bounced back away from the enemy. This is essentially the basic enemy variants attack. The plan is to make it so when the player is bounced backwards, they will lose 1hp.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,6 +8,7 @@ public class Enemy : MonoBehaviour
     public float speed = 0.1f;
     private Rigidbody enemyRB;
     private GameObject player;
+    private float knockBackStrength = 80f;
 
 
     // Start is called before the first frame update
@@ -23,5 +24,18 @@ public class Enemy : MonoBehaviour
     {
         // Enemy follows player
         enemyRB.AddForce((player.transform.position - transform.position).normalized * speed);
+    }
+
+    // Makes it so when the enemy runs into the player, the player will get bounced back
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Rigidbody playerRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromEnemy = (collision.gameObject.transform.position - transform.position);
+
+            Debug.Log("Collided with: " + collision.gameObject.name);
+            playerRigidbody.AddForce(awayFromEnemy * knockBackStrength, ForceMode.Impulse);
+        }
     }
 }
